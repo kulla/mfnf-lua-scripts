adf85db79e7a2fdc3db36c8bca936030803ac61d
Stephan Kulla <git.mail@kulla.me>
2016-09-13 00:01:49 +0200

    Utils.lua: Format function ends().
    Signed-off-by: Stephan Kulla <git.mail@kulla.me>

--- src/Utils.lua
+++ src/Utils.lua
@@ -4,8 +4,8 @@ function utils.starts(str, start)
   return string.sub(str, 1, string.len(start)) == start
 end
 
-function utils.ends(str,ending)
-  return ending == '' or string.sub(str,-string.len(ending)) == ending
+function utils.ends(str, ending)
+  return ending == '' or string.sub(str, -string.len(ending)) == ending
 end
 
 return utils
