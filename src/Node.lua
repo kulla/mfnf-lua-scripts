9e711e5f5b62c0a0374fbcd5c96f23f51a8c91be
Vivian Haller <vivian.haller@haller-software.at>
2016-09-14 08:57:17 +0200

    added the function node:add() and a corresponding unit test.

--- src/Node.lua
+++ src/Node.lua
@@ -5,4 +5,10 @@ function node:new()
   return setmetatable({parent = nil, children = {}}, node)
 end
 
+function node:add_child(child)
+  self.children[#self.children + 1] = child
+  child.parent = self
+  child.index  = #self.children
+end
+
 return node
