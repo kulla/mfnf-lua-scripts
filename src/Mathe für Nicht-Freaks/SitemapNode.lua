79cb402ae8a16d1b9860a7cece3c846083cb5cf1
Stephan Kulla <git.mail@kulla.me>
2017-01-03 13:16:31 +0100

    Add constructor SitemapNode:new()
    Signed-off-by: Stephan Kulla <git.mail@kulla.me>

--- src/Mathe für Nicht-Freaks/SitemapNode.lua
+++ src/Mathe für Nicht-Freaks/SitemapNode.lua
@@ -1,8 +1,20 @@
 Node = require("Node")
 
 local SitemapNode = {}
+SitemapNode.__index = SitemapNode
 
 -- SitemapNode shall inherit from Node
 setmetatable(SitemapNode, { __index = Node })
+
+function SitemapNode:new(link, name, weight, progress)
+  local self = Node:new()
+
+  self.weight   = weight
+  self.link     = link
+  self.name     = name
+  self.progress = progress
+
+  return setmetatable(self, SitemapNode)
+end
 
 return SitemapNode
--- test/Mathe für Nicht-Freaks/SitemapNode_test.lua
+++ test/Mathe für Nicht-Freaks/SitemapNode_test.lua
@@ -7,4 +7,18 @@ function TestSitemapNode:test_inheritance()
   luaunit.assertIs(getmetatable(SitemapNode).__index, Node)
 end
 
+function TestSitemapNode:test_new()
+  local node = SitemapNode:new("<link>", "<name>", 23, 42)
+
+  luaunit.assertNotNil(node)
+  luaunit.assertNil(node.parent)
+  luaunit.assertEquals(node.children, {})
+  luaunit.assertIs(getmetatable(node), SitemapNode)
+
+  luaunit.assertEquals(node.weight, 23)
+  luaunit.assertEquals(node.link, "<link>")
+  luaunit.assertEquals(node.name, "<name>")
+  luaunit.assertEquals(node.progress, 42)
+end
+
 return TestSitemapNode
